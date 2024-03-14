39cff9b0bbe219b87f8dddaf1c119422e420bc52
wacherawaweru <wacheram22@gmail.com>
2024-03-14 15:42:55 -0700

    corrections

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-     a = 10
-    b = 5
-
     from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
 
     result_add = add(a, b)
     result_sub = sub(a, b)
