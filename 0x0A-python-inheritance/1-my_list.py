d4fb2ba2e46a0a8f804f39a295329ba441a7fb91
wacherawaweru <wacheram22@gmail.com>
2024-04-17 05:40:32 -0700

    MYlist from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''1-my_list.py
+'''
+
+
+class MyList(list):
+    """
+    A subclass of the built-in list class that provides additional functionality.
+    """
+
+    def print_sorted(self):
+        """
+        Print the list in sorted order (ascending).
+
+        This method sorts the elements of the list in ascending order
+        and prints the sorted list.
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
--- 0x0A-python-inheritance/README.md
+++ 0x0A-python-inheritance/README.md
@@ -4,4 +4,7 @@ Tasks
 0.  a function that returns the list of available attributes and methods of an object:
 Prototype: def lookup(obj):
 Returns a list object
-1.
+1.a class MyList that inherits from list:
+
+Public instance method: def print_sorted(self): that prints the list, but sorted (ascending sort)
+You can assume that all the elements of the list will be of type int
