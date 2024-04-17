c097fba5e4cb7c25c9365540391b6165c0869293
wacherawaweru <wacheram22@gmail.com>
2024-04-17 06:36:38 -0700

    with public instance attr

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+6-base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """
+    Base class representing geometry.
+
+    This class serves as a base for other geometry-related classes.
+    """
+
+    def area(self):
+        """
+        Calculate the area.
+
+        Raises:
+            Exception: area() is not implemented.
+        """
+        raise Exception("area() is not implemented")
