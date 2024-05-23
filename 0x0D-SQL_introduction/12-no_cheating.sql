fd94ca9c00688b2efd1bbd1202b1657976d6b097
wacherawaweru <wacheram22@gmail.com>
2024-05-23 16:46:37 -0700

    SQL intro

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,5 @@
--- Updates the score of Bob to 10 in the table second_table without using Bob's id value
-UPDATE hbtn_0c_0.second_table SET score = 10 WHERE name = 'Bob';
+-- update score of Bob to 10
+UPDATE `second_table`
+SET
+`score` = 10
+WHERE `second_table` . `name` = 'Bob';
--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,6 @@
 -- Prints the full description of the table first_table from the database hbtn_0c_0
-DESCRIBE hbtn_0c_0.first_table;
+-- Prints the full description of the table first_table from the database hbtn_0c_0
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
 
