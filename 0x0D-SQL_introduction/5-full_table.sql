67cc83dc9ca7aec371012eb316704bfdd9eb483f
wacherawaweru <wacheram22@gmail.com>
2024-05-23 16:52:13 -0700

    sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,2 @@
--- Prints the full description of the table first_table from the database hbtn_0c_0
--- Prints the full description of the table first_table from the database hbtn_0c_0
-SELECT COLUMN_NAME, COLUMN_TYPE
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
-
+-- Prints the full description of first_table
+SHOW CREATE table first_table;
