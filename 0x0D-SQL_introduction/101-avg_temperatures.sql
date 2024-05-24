a514c3df411395ac93ee2f6bc35cf89ec97b88a5
wacherawaweru <wacheram22@gmail.com>
2024-05-24 04:30:42 -0700

    sql corrected

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,4 @@
 -- Import the provided table dump into the hbtn_0c_0 database
 -- (Assuming the table dump file is named temperatures_table_dump.sql)
-SOURCE /path/to/temperatures_table_dump.sql;
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,11 +1,10 @@
 -- Display the top 3 cities' temperatures during July and August ordered by temperature (descending)
 CREA TABLE IF NOT EXISTS temp_july_aug
 SELECT *
-FROM temperatures WHERE mont =7 OR month = 8;
+FROM temperatures WHERE month =7 OR month = 8;
 
 SELECT city, AVG(temp) AS avg_temp
-FROM temperatures
-WHERE month IN (7, 8)
+FROM temp_july_aug
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
