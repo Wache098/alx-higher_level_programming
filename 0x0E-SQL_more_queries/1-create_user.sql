c7437078bd3ab1c88e096a3d56100319fad06979
wacherawaweru <wacheram22@gmail.com>
2024-05-28 13:04:10 -0700

    more sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,8 @@
-__create the user user_0d_1 with the specified password
-CREATE IF NOT EXISTS 'user_0d_1'@ 'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Create the user user_0d_1 with the specified password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
-__grant all privileges to the user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTIONS;
+-- grant all privileges to the user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
-__flush the privileges to ensure that they are applied
+-- flush the privileges to ensure that they are applied
 FLUSH PRIVILEGES;
--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,11 @@
-__create table force_name if not exists
-CREATE TABLE force_name IF NOT EXISTS(
+-- create the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- switch to the database 
+USE hbtn_0d_2;
+
+-- create table force_name if not exists
+CREATE TABLE IF NOT EXISTS force_name (
 	id INT,
-	name VARCHAR(256) NOT NULL,
+	name VARCHAR(256) NOT NULL
 	);
