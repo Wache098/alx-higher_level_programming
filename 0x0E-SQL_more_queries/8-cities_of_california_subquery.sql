ab49f9e414c0c622554b5b98260b98291b484c7c
wacherawaweru <wacheram22@gmail.com>
2024-05-28 13:39:05 -0700

    more

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,17 +1,8 @@
--- use database hbtn_0d_usa
+-- Use the hbtn_0d_usa database
 USE hbtn_0d_usa;
--- Select all rows from the cities table
-SELECT * 
--- Filter the rows based on the state_id
-FROM cities 
--- Use a subquery to find the id of the state with the name 'California'
-WHERE state_id = (
-    -- Subquery to get the id of the state 'California'
-    SELECT id 
-    -- From the states table
-    FROM states 
-    -- Where the name is 'California'
-    WHERE name = 'California'
-)
--- Order the results by city id
-ORDER BY id;
+
+-- Find the ID of the state 'California'
+SET @california_id = (SELECT id FROM states WHERE name = 'California');
+
+-- List all cities of California using a subquery
+SELECT * FROM cities WHERE state_id = @california_id ORDER BY id;
