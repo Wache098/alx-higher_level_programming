628d3e840be862bfe5064b0aace3fdd3dc36ff1d
wacherawaweru <wacheram22@gmail.com>
2024-05-28 13:09:32 -0700

    corrected

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -9,3 +9,11 @@ CREATE TABLE IF NOT EXISTS force_name (
 	id INT,
 	name VARCHAR(256) NOT NULL
 	);
+
+-- Insert data into the table
+INSERT INTO force_name (id, name) VALUES
+(1, 'Holberton School'),
+(1, 'Python is cool'),
+(2, 'Holberton'),
+(3, 'School'),
+(4, 'C is fun');
