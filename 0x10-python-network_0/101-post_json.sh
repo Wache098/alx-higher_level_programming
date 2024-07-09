e0a87a9aee4fd5004d3b0749d2b0e959687cf3c8
wacherawaweru <wacheram22@gmail.com>
2024-07-09 14:28:47 -0700

    revised

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
-# Script that sends a JSON POST request to a URL and displays the body of the response
-
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <URL> <JSON_FILE>"
-    exit 1
-fi
-
-url=$1
-json_file=$2
-
-# Validate if the JSON file is valid
-if ! jq '.' "$json_file" >/dev/null 2>&1; then
-    echo "Not a valid JSON"
-    exit 1
-fi
-
-# Send POST request with JSON content
-response=$(curl -s -X POST -H "Content-Type: application/json" -d @"$json_file" "$url")
-
-# Display the body of the response
-echo "$response"
+# Sends a JSON POST request to a given URL with a given JSON file.
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Script that makes a request to cause the server to respond with "You got me!"
-
-# Send a POST request to the server with specific headers
-curl -sX PUT -d "user_id=98" 0.0.0.0:5000/catch_me -L
-
+# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
