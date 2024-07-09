3b39050dce30df32a8b0931cab831aab24f18ca0
wacherawaweru <wacheram22@gmail.com>
2024-07-09 13:38:35 -0700

    1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-# Sends a GET request to the URL and displays the body of a 200 status code response
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
+#!/bin/bash                                                                   
+# a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response                                                  
+if [ "$(curl -sLI "$1" -X GET | grep "200 OK" | cut -d' ' -f2)" = '200' ]; then curl -sL "$1"; fi
